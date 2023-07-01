b6e907ecc7070ec6613212994ada05a360016bef
Sachin Munda <sachinmunda954@gmail.com>
2023-07-01 17:02:17 +0530

    start

---
+++ public/client.js
@@ -0,0 +1,56 @@
+ const socket = io()
+
+ //message logic
+
+let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+
+ do{
+    name = prompt('please enter your name: ')
+ }while(!name)
+
+ textarea.addEventListener('keyup', (e) => {
+    if(e.key ==='Enter'){
+        sendMessage(e.target.value)
+    }
+ })
+
+ function sendMessage(message){
+        let msg = {
+        user: name,
+        message: message.trim()
+    }
+    //Append
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    //send to sever via web socket connection
+    socket.emit('message', msg)
+ }
+
+ function appendMessage(msg, type){
+    let mainDiv = document.createElement('div')
+    let className = type;
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+
+    mainDiv.innerHTML = markup;
+    messageArea.appendChild(mainDiv)
+ }
+
+ //Recieve message
+
+ socket.on('message', (msg) =>{
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+ })
+
+ function scrollToBottom(){
+    messageArea.scrollTop = messageArea.scrollHeight
+ }
