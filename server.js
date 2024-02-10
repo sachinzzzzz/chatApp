3aa342d52d011398294f24def6ab0a4819fe0f8c
sachinzzzzz <sachinmunda954@gmail.com>
2024-02-10 21:08:13 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = express()
 const http = require('http').createServer(app)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 http.listen(PORT , () => {
     console.log(`Listening ${PORT}`)
